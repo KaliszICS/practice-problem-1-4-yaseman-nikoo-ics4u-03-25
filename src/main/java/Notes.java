c299bed8262038b888a09c052d412046be731f1a
yaseman-nikoo-ics4u-03-25 <yaseman.nikoo@student.tdsb.on.ca>
2025-03-19 17:50:43 +0000

    1.4 compelte

--- src/main/java/Notes.java
+++ src/main/java/Notes.java
@@ -3,51 +3,51 @@ import java.io.*;
 class Notes {
 	public static void main(String[] args) {
 
-		// FileWriter fw = null;
+		FileWriter fw = null;
 
-		// try {
-		// 	fw = new FileWriter("example.txt"); //Creates a new file, replaces the old one if it already exists
+		try {
+			fw = new FileWriter("example.txt"); //Creates a new file, replaces the old one if it already exists
 			
-		// 	fw.write("Hello\n"); //more similar to print than println (does not add a newline)
-		// 	fw.write("Bye");
-		// }
-		// catch (IOException e) {
-		// 	System.out.println(e);
-		// }
-		// finally { //always run no matter what.  even if you returned in the try, it still runs
-		// 	try {
-		// 		if (fw != null) {
-		// 			fw.close();
-		// 		}
-		// 	}
-		// 	catch (IOException e) {
-		// 		System.out.println(e);
-		// 	}
-		// }
+			fw.write("Hello\n"); //more similar to print than println (does not add a newline)
+			fw.write("Bye");
+		}
+		catch (IOException e) {
+			System.out.println(e);
+		}
+		finally { //always run no matter what.  even if you returned in the try, it still runs
+			try {
+				if (fw != null) {
+					fw.close();
+				}
+			}
+			catch (IOException e) {
+				System.out.println(e);
+			}
+		}
 
-		// BufferedWriter bw = null;
+		BufferedWriter bw = null;
 
 
-		// try {
-		// 	bw = new BufferedWriter(new FileWriter("example2.txt", true));
-		// 	//adding the true boolean after, changes it from overwriting to appending to the file.
+		try {
+			bw = new BufferedWriter(new FileWriter("example2.txt", true));
+			//adding the true boolean after, changes it from overwriting to appending to the file.
 
-		// 	bw.write("Hello\n");
-		// 	bw.write("Bye");
-		// }
-		// catch(IOException e) {
-		// 	System.out.println(e);
-		// }
-		// finally {
-		// 	try {
-		// 		if (bw != null) {
-		// 			bw.close();
-		// 		}
-		// 	}
-		// 	catch (IOException e) {
-		// 		System.out.println(e);
-		// 	}
-		// }
+			bw.write("Hello\n");
+			bw.write("Bye");
+		}
+		catch(IOException e) {
+			System.out.println(e);
+		}
+		finally {
+			try {
+				if (bw != null) {
+					bw.close();
+				}
+			}
+			catch (IOException e) {
+				System.out.println(e);
+			}
+		}
 
 		PrintWriter pw = null;
 
--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -65,4 +65,54 @@ public class PracticeProblem {
 		return age;
 		
 	}
+	public static int getNumber(int Line, String file){
+		int number =-1;
+		Scanner s = null;
+		try{
+			
+			s = new Scanner(new BufferedReader(new FileReader(file)));
+		int counter = 1;
+		while (s.hasNext() && counter < Line){
+			s.nextLine();
+			counter++;
+		}
+
+			if (counter == Line && s.hasNext()){
+				s.next();
+				s.next();
+				s.next();
+				number= s.nextInt();
+			}	
+		}
+		catch(IOException e){
+			System.out.println(e);
+		}
+		finally {
+			if (s != null) {
+				s.close();
+			}
+		}
+		return number;
+		
+	}
+	public static void fileAppend(String output, String filename){
+		BufferedWriter fw = null;
+		try{
+			fw = new BufferedWriter(new FileWriter(filename, true));
+			fw.write(output);
+		}
+		catch(IOException e){
+			System.out.println(e);
+		}
+		finally{
+			try{
+				if (fw!=null){
+					fw.close();
+				}
+			}
+			catch(IOException e){
+				System.out.println(e);
+			}
+		}
+	}
 }
